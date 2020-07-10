1c839cda26418ebf07b83e94fd9762a132a04dfd
pad19 <pad19051999@gmail.com>
2020-07-10 20:12:34 +0300

    Add set get to classes Fuel and Car

--- scrips/index.js
+++ scrips/index.js
@@ -1,44 +1,26 @@
 'use strict';
 
-class Worker {
-    constructor(name, surname, rate = 0, days = 0) {
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
+class Fuel {
+    constructor(volume, density) {
+        this.volume = volume;
+        this.density = density;
     }
 
-    set rate(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('param rate must be a number');
-        }
-        if (value < 0) {
-            throw new RangeError('rate must be a positive number');
-        }
-        this._rate = value;
-    }
-
-    get rate() {
-        return this._rate;
-    }
-
-    set days(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('param days must be a number');
-        }
-        if (value < 0 || !Number.isInteger(value) || value > 31) {
-            throw new RangeError('days must be a positive integer number');
-        }
-        this._days = value;
-    }
-
-    get days() {
-        return this._days;
-    }
-
-    getSalary() {
-        return this.rate * this.days;
+    get weight() {
+        return this.density * this.volume;
     }
 }
 
-const worker1 = new Worker('Ivan', 'Ivanov', 100, 30);
+class Car {
+    constructor(weight, fuel) {
+        this.weight = weight;
+        this.fuel = fuel;
+    }
+
+    get fullWeight() {
+        return this.weight + this.fuel.weight;
+    }
+}
+
+const VW = new Car(1400, new Fuel(100, 0.86));
+console.log(VW.fullWeight);
