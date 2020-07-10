3b2d6ddd8d3c83c88a35878366ee993e5f67aa93
pad19 <pad19051999@gmail.com>
2020-07-10 18:40:27 +0300

    Add isAdult to class Worker

--- scrips/index.js
+++ scrips/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 class Worker {
-    constructor(name, surname, rate = 0, days = 0) {
+    constructor(name, surname, age, rate = 0, days = 0) {
         this.name = name;
         this.surname = surname;
+        this.age = age;
         this.rate = rate;
         this.days = days;
+    }
+
+    isAdult() {
+        return this.age > 17;
     }
 
     getSalary() {
@@ -13,5 +18,6 @@ class Worker {
     }
 }
 
-const worker1 = new Worker('Ivan', 'Ivanov', 100, 30);
+const worker1 = new Worker('Ivan', 'Ivanov', 17, 100, 30);
 console.log(worker1.getSalary());
+console.log(worker1.isAdult());
